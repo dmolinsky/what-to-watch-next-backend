31de6b18e1dd95169630fce5892ea836a7fa3906
dmolinsky <diana.molinsky@iths.se>
2026-01-03 12:43:50 +0100

    update: pom config

---
+++ src/main/java/se/dmolinsky/whattowatchnextbackend/controller/HealthController.java
@@ -0,0 +1,4 @@
+package se.dmolinsky.whattowatchnextbackend.controller;
+
+public class HealthController {
+}
---
+++ src/main/java/se/dmolinsky/whattowatchnextbackend/domain/Title.java
@@ -0,0 +1,4 @@
+package se.dmolinsky.whattowatchnextbackend.domain;
+
+public class Title {
+}
