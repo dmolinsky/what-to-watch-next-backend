823c83e8cb32b529a54dae2406466c5262176487
dmolinsky <diana.molinsky@iths.se>
2026-01-03 10:59:32 +0100

    chore: initial gitignore

---
+++ src/main/java/se/dmolinsky/whattowatchnextbackend/WhatToWatchNextBackendApplication.java
@@ -0,0 +1,13 @@
+package se.dmolinsky.whattowatchnextbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToWatchNextBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatToWatchNextBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/dmolinsky/whattowatchnextbackend/WhatToWatchNextBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.dmolinsky.whattowatchnextbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatToWatchNextBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
